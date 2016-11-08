da15c94d832c6450600b479eb4f115816f2f3afe
Erica Y. Scott <eyscott@ucdavis.edu>
2016-11-08 11:37:55 -0800

    Update lncRNA_filtering.R

--- lncRNA_filtering.R
+++ lncRNA_filtering.R
@@ -1,4 +1,4 @@
-##lncRNA filtering pipeline R##
+##lncRNA pipeline R##
 setwd("~/Desktop/lncRNA")
 overallExpression <- read.table("dataSummary", header=T, stringsAsFactors=F)
 
@@ -16,9 +16,9 @@ intergenic
 
 #slim down to columns you want
 keeps <- c("transcriptName","length", "calcTPM")
-novel_I <- novel_I[keeps]
-novel_II <- novel_II[keeps]
-novel_III <- novel_III[keeps]
+novel_I <- unique(novel_I[keeps])
+novel_II <- unique(novel_II[keeps])
+novel_III <- unique(novel_III[keeps])
 intergenic
 
 #filter out transcripts <200 nt and with TPM < 0.1
@@ -33,21 +33,21 @@ novel_II_bed <- merge(novel_II_f1, unfiltered_bed, by.x="transcriptName",by.y="V
 novel_III_bed <- merge(novel_III_f1, unfiltered_bed, by.x="transcriptName",by.y="V4" )
 
 #reorder for proper bed format
-novel_I_bed <- novel_I_bed[ ,c("V4","V2","V3","transcriptName","V5","V6","V7","V8","V9","V10","V11","V12")] 
+novel_I_bed <- novel_I_bed[ ,c("V1","V2","V3","transcriptName","V5","V6","V7","V8","V9","V10","V11","V12")] 
 names(novel_I_bed)[4]<-paste("V4")
-novel_II_bed <- novel_II_bed[ ,c("V4","V2","V3","transcriptName","V5","V6","V7","V8","V9","V10","V11","V12")] 
+novel_II_bed <- novel_II_bed[ ,c("V1","V2","V3","transcriptName","V5","V6","V7","V8","V9","V10","V11","V12")] 
 names(novel_II_bed)[4]<-paste("V4")
-novel_III_bed <- novel_III_bed[ ,c("V4","V2","V3","transcriptName","V5","V6","V7","V8","V9","V10","V11","V12")] 
+novel_III_bed <- novel_III_bed[ ,c("V1","V2","V3","transcriptName","V5","V6","V7","V8","V9","V10","V11","V12")] 
 names(novel_III_bed)[4]<-paste("V4")
 #order chr properly
-novel_I_bed <- novel_I_bed[with(novel_I_bed, order(V4, V2)), ]
-novel_II_bed <- novel_II_bed[with(novel_II_bed, order(V4, V2)), ]
-novel_III_bed <- novel_III_bed[with(novel_III_bed, order(V4, V2)), ]
+novel_I_bed <- novel_I_bed[with(novel_I_bed, order(V1, V2)), ]
+novel_II_bed <- novel_II_bed[with(novel_II_bed, order(V1, V2)), ]
+novel_III_bed <- novel_III_bed[with(novel_III_bed, order(V1, V2)), ]
 
 #write the bedfiles
-write.table(novel_I_bed, "novel_I.bed", row.names=F, col.names=F)
-write.table(novel_II_bed, "novel_II.bed", row.names=F, col.names=F)
-write.table(novel_III_bed, "novel_III.bed", row.names=F, col.names=F)
+write.table(novel_I_bed, "novel_I.bed", row.names=F, col.names=F, quote=F, sep = "\t")
+write.table(novel_II_bed, "novel_II.bed", row.names=F, col.names=F, quote=F, sep = "\t")
+write.table(novel_III_bed, "novel_III.bed", row.names=F, col.names=F, quote=F, sep = "\t")
 
 #minus 1 kb and add 1 kb to end of each gene for merged transcriptome
 merged_bed <- read.table("mergedTrans.bed", header=F,stringsAsFactors=F )
@@ -60,7 +60,7 @@ names(merged_bed_extended)[3]<-paste("V3")
 merged_bed_extended$V2[merged_bed_extended$V2 < 0] <- 0
 #order chr properly
 merged_bed_extended <- merged_bed_extended[with(merged_bed_extended, order(V1, V2)), ]
-write.table(merged_bed_extended, "merged_extended.bed", row.names=F, col.names=F)
+write.table(merged_bed_extended, "merged_extended.bed", row.names=F, col.names=F, quote=F,, sep = "\t")
 #this is to allow for removal of any transcripts within 1 kb of an annotated gene
 
 #Now to isolate the -1000 (5'UTR)
@@ -69,11 +69,9 @@ merged_bed_5 <- merged_bed[ ,c("V1","minus","V2","V4","V5","V6","V7","V8","V9","
 merged_bed_5$minus[merged_bed_5$minus < 0] <- 0
 #order chr properly
 merged_bed_5 <- merged_bed_5[with(merged_bed_5, order(V1, minus)), ]
-write.table(merged_bed_5, "merged_5.bed", row.names=F, col.names=F)
+write.table(merged_bed_5, "merged_5.bed", row.names=F, col.names=F, quote=F, sep = "\t")
 #Now to isolate the +1000 (3'UTR)
 merged_bed["add"]<- (merged_bed[ ,3] + 1000)
 merged_bed_3 <- merged_bed[ ,c("V1","V3","add","V4","V5","V6","V7","V8","V9","V10","V11","V12")]
 merged_bed_3 <- merged_bed_3[with(merged_bed_3, order(V1, V3)), ]
-write.table(merged_bed_3, "merged_3.bed", row.names=F, col.names=F)
-
-##use bedtool intersect function!
+write.table(merged_bed_3, "merged_3.bed", row.names=F, col.names=F, quote=F, sep = "\t")
