1e3f8c0cebf44b17a955a8b3299d97f8110e6ab6
Erica Y. Scott <eyscott@ucdavis.edu>
2016-12-04 12:27:41 -0800

    Update 3_lncRNA_filter3_bedtools.sh

--- 3_lncRNA_filter3_bedtools.sh
+++ 3_lncRNA_filter3_bedtools.sh
@@ -1,11 +1,12 @@
 ##now use bedtools
 module load BEDTools/2.24.0  
-bedtools intersect -c -a novel_I_f2.bed -b merged_5.bed -sorted > novel_I_5.bed
-bedtools intersect -c -a novel_II_f2.bed -b merged_5.bed -sorted > novel_II_5.bed
-bedtools intersect -c -a novel_III_f2.bed -b merged_5.bed -sorted > novel_III_5.bed
-bedtools intersect -c -a intergenic_f2.bed -b merged_5.bed -sorted > intergenic_5.bed
+bedtools intersect -c -a novel_I_f2.bed -b merged_5.bed -sorted -s > novel_I_5.bed
+bedtools intersect -c -a novel_II_f2.bed -b merged_5.bed -sorted -s > novel_II_5.bed
+bedtools intersect -c -a novel_III_f2.bed -b merged_5.bed -sorted -s > novel_III_5.bed
+bedtools intersect -c -a intergenic_f2.bed -b merged_5.bed -sorted -s > intergenic_5.bed
 
-bedtools intersect -c -a novel_I_f2.bed -b merged_3.bed -sorted > novel_I_3.bed
-bedtools intersect -c -a novel_II_f2.bed -b merged_3.bed -sorted > novel_II_3.bed
-bedtools intersect -c -a novel_III_f2.bed -b merged_3.bed -sorted > novel_III_3.bed
-bedtools intersect -c -a intergenic_f2.bed -b merged_3.bed -sorted > intergenic_3.bed
+
+bedtools intersect -c -a novel_I_f2.bed -b merged_3.bed -sorted -s > novel_I_3.bed
+bedtools intersect -c -a novel_II_f2.bed -b merged_3.bed -sorted -s > novel_II_3.bed
+bedtools intersect -c -a novel_III_f2.bed -b merged_3.bed -sorted -s > novel_III_3.bed
+bedtools intersect -c -a intergenic_f2.bed -b merged_3.bed -sorted -s > intergenic_3.bed
