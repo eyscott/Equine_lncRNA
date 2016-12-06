b5c2cbb63497290f2a336d9b28c4dfcad87f7b10
drtamermansour <drtamermansour@gmail.com>
2016-12-06 13:50:47 -0500

    fix module loading

--- 5_lncRNA_filter4_bedtools.sh
+++ 5_lncRNA_filter4_bedtools.sh
@@ -1,4 +1,5 @@
 ##now use bedtools
+module load GNU/4.4.5 
 module load BEDTools/2.24.0  
 bedtools intersect -c -a novel_I_f3.bed -b refined_nolncRNA_5.bed -sorted -s -f 0.25 > novel_I_5.bed
 bedtools intersect -c -a novel_II_f3.bed -b refined_nolncRNA_5.bed -sorted -s -f 0.25 > novel_II_5.bed
