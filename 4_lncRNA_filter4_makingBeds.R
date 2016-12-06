3de49b9682b598e8321000cee983f9f1a07fd9fe
Erica Y. Scott <eyscott@ucdavis.edu>
2016-12-06 09:46:07 -0800

    Update 4_lncRNA_filter4_makingBeds.R

--- 4_lncRNA_filter4_makingBeds.R
+++ 4_lncRNA_filter4_makingBeds.R
@@ -2,11 +2,11 @@ setwd("~/Dropbox/Horse_Transcriptome/downloads")
 refined_bed <- read.table("allTissues_BED/refined.bed", header=F, stringsAsFactors=F)
 refined <- refined_bed[ ,c("V1","V2","V3","V4")]
 setwd("~/Dropbox/lncRNA")
-f3 <- read.table("lncRNA_f3_IDs", header=F, stringsAsFactors=F)
+f3 <- read.table("lncRNA_f3_IDs", header=F, stringsAsFactors=F)[-1]
 #make sure the mergedtrans has none of the novel I,II,III lncRNA
 library(dplyr)
-refined_nolncRNA <- anti_join(refined,f3)
-refined_nolncRNA_bed <- anti_join(refined_bed,f3)
+names(f3) <- names(refined_bed)
+refined_nolncRNA_bed <- anti_join(refined_bed,f3,by="V4")
 #order chr properly
 refined_nolncRNA_bed <- refined_nolncRNA_bed[with(refined_nolncRNA_bed, order(V1, V2)), ]
 setwd("~/Dropbox/lncRNA")
@@ -30,7 +30,7 @@ write.table(refined_nolncRNA_bed_extended, "refined_nolncRNA_bed_extended.bed", 
 #this is to allow for removal of any transcripts within 1 kb of an annotated gene
 #Now to isolate the -1000 (5'UTR)
 refined_nolncRNA_bed_5 <- refined_nolncRNA_bed[ ,c("V1","minus","V2","V4","V5","V6","V7","V8","V9","V10","V11","V12")]
-refined_nolncRNA_bed_5$minus[refined_nolncRNA_5$minus < 0] <- 0
+refined_nolncRNA_bed_5$minus[refined_nolncRNA_bed_5$minus < 0] <- 0
 #order chr properly
 refined_nolncRNA_bed_5 <- refined_nolncRNA_bed_5[with(refined_nolncRNA_bed_5, order(V1, minus)), ]
 write.table(refined_nolncRNA_bed_5, "refined_nolncRNA_5.bed", row.names=F, col.names=F, quote=F, sep = "\t")
