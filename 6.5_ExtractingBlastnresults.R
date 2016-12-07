404c17b17d7421152e9731c2ddb6e6df855f9e4b
Erica Y. Scott <eyscott@ucdavis.edu>
2016-12-06 22:58:22 -0800

    Update 6.5_ExtractingBlastnresults.R

--- 6.5_ExtractingBlastnresults.R
+++ 6.5_ExtractingBlastnresults.R
@@ -6,11 +6,12 @@ P_blastn_temp2=data.frame(str_split_fixed(P_blastn_temp1$X2, "\\(", 2))
 P_blastn_temp3=data.frame(str_split_fixed(P_blastn_temp2$X1, "-", 2))
 P_blastn_T_bed <- cbind(P_blastn_temp1$X1,P_blastn_temp3,P_blastn[,c("V2","V4")])
 names(P_blastn_T_bed)=c("chr","start","stop","name","V4")
-P_blastn_bed <- merge(P_blastn_T_bed,P_noDups_id, by=c("chr","start","stop"))
-P_blastn_bed <- P_blastn_bed[ ,c("id","chr","start","stop","V4.y","V5","V6","V7","V8","V9","V10","V11","V12")]
 
 P_bed <- read.table("P_id.bed", header=F, stringsAsFactors=F,sep = "\t")
 names(P_bed) <- c("chr","start","stop","id","V4","V5","V6","V7","V8","V9","V10","V11","V12")
+
+P_blastn_bed <- merge(P_blastn_T_bed,P_bed, by=c("chr","start","stop"))
+P_blastn_bed <- P_blastn_bed[ ,c("id","chr","start","stop","V4.y","V5","V6","V7","V8","V9","V10","V11","V12")]
 
 P_lncRNA <- subset(P_blastn_T_bed, V4 >20)#makes a threshold of 20% coverage
 P_lncRNA <- merge(P_lncRNA,P_bed, by=c("chr","start","stop"))
