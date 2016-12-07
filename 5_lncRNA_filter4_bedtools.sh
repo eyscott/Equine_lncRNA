d8033e512c3dd7941b89695bff7cef77b22f96d1
drtamermansour <drtamermansour@gmail.com>
2016-12-07 00:10:14 -0500

    minor erros

--- 5_lncRNA_filter4_bedtools.sh
+++ 5_lncRNA_filter4_bedtools.sh
@@ -1,11 +1,11 @@
 ##now use bedtools
 module load GNU/4.4.5 
 module load BEDTools/2.24.0  
-bedtools intersect -c -a novel_I_f3.bed -b refined_nolncRNA_5.bed -sorted -s -f > novel_I_5.bed
-bedtools intersect -c -a novel_II_f3.bed -b refined_nolncRNA_5.bed -sorted -s -f > novel_II_5.bed
-bedtools intersect -c -a novel_III_f3.bed -b refined_nolncRNA_5.bed -sorted -s -f > novel_III_5.bed
-bedtools intersect -c -a intergenic_f3.bed -b refined_nolncRNA_5.bed -sorted -s -f 0.25 > intergenic_5.bed
-bedtools intersect -c -a known_ncRNA_f3.bed -b refined_nolncRNA_5.bed -sorted -s -f 0.25 > known_5.bed
+bedtools intersect -c -a novel_I_f3.bed -b refined_nolncRNA_5.bed -sorted -s > novel_I_5.bed
+bedtools intersect -c -a novel_II_f3.bed -b refined_nolncRNA_5.bed -sorted -s > novel_II_5.bed
+bedtools intersect -c -a novel_III_f3.bed -b refined_nolncRNA_5.bed -sorted -s > novel_III_5.bed
+bedtools intersect -c -a intergenic_f3.bed -b refined_nolncRNA_5.bed -sorted -s > intergenic_5.bed
+bedtools intersect -c -a known_ncRNA_f3.bed -b refined_nolncRNA_5.bed -sorted -s > known_5.bed
 
 bedtools intersect -c -a novel_I_f3.bed -b refined_nolncRNA_3.bed -sorted -s > novel_I_3.bed
 bedtools intersect -c -a novel_II_f3.bed -b refined_nolncRNA_3.bed -sorted -s > novel_II_3.bed
