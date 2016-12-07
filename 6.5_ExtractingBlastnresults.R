3c6f58f5d44006107b596ce11c0915c22a93b77a
Erica Y. Scott <eyscott@ucdavis.edu>
2016-12-06 22:51:28 -0800

    Update 6.5_ExtractingBlastnresults.R

--- 6.5_ExtractingBlastnresults.R
+++ 6.5_ExtractingBlastnresults.R
@@ -9,8 +9,11 @@ names(P_blastn_T_bed)=c("chr","start","stop","name","V4")
 P_blastn_bed <- merge(P_blastn_T_bed,P_noDups_id, by=c("chr","start","stop"))
 P_blastn_bed <- P_blastn_bed[ ,c("id","chr","start","stop","V4.y","V5","V6","V7","V8","V9","V10","V11","V12")]
 
+P_bed <- read.table("P_id.bed", header=F, stringsAsFactors=F,sep = "\t")
+names(P_bed) <- c("chr","start","stop","id","V4","V5","V6","V7","V8","V9","V10","V11","V12")
+
 P_lncRNA <- subset(P_blastn_T_bed, V4 >20)#makes a threshold of 20% coverage
-P_lncRNA <- merge(P_lncRNA,P_noDups_id, by=c("chr","start","stop"))
+P_lncRNA <- merge(P_lncRNA,P_bed, by=c("chr","start","stop"))
 P_lncRNA <- P_lncRNA[ ,c("id","chr","start","stop","V4.y","V5","V6","V7","V8","V9","V10","V11","V12")]
 P_lncRNA <- P_lncRNA[!duplicated(P_lncRNA$V4.y),] #amounts to 264 transcripts
 write.table(P_blastn_bed, "P_lncRNAall.bed", row.names=F, col.names=F, quote=F, sep = "\t")
