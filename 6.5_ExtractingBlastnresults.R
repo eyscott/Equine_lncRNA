a3cd3577a4ffd94a3371e947e3c0780236c25f89
drtamermansour <drtamermansour@gmail.com>
2016-12-08 01:25:46 -0500

    script 6 and 6.5

--- 6.5_ExtractingBlastnresults.R
+++ 6.5_ExtractingBlastnresults.R
@@ -8,12 +8,13 @@ names(P_blastn)=c("V5","sseqid","pident","qcovs","evalue")
 P_noDups_id <- read.table("P_id.bed", header=F, stringsAsFactors=F,sep = "\t")
 P_blastn_All <- merge(P_blastn,P_noDups_id, by="V5")
 
-P_blastn_bed <- P_blastn_All[ ,c("V1","V2","V3","V4","V5","V6","V7","V8","V9","V10","V11","V12","V13")]
+P_blastn_bed <- P_blastn_All[ ,colnames(P_noDups_id)]
 
 P_lncRNA <- subset(P_blastn_All, qcovs >20)#makes a threshold of 20% coverage
-P_lncRNA <- P_lncRNA[ ,c("V1","V2","V3","V4","V5","V6","V7","V8","V9","V10","V11","V12","V13")] #amounts to 264 transcripts
+P_lncRNA <- P_lncRNA[ ,colnames(P_noDups_id)] 
+P_lncRNA <- P_lncRNA[!duplicated(P_lncRNA),] ## 134 transcript
 
-P_final_bed <- anti_join(P_noDups_id,P_lncRNA,by=c("V1","V2","V3","V4"))
+P_final_bed <- anti_join(P_noDups_id,P_lncRNA,by="V4")
 
 write.table(P_blastn_bed, "P_lncRNAall.bed", row.names=F, col.names=F, quote=F, sep = "\t")
 write.table(P_lncRNA, "P_lncRNA.bed", row.names=F, col.names=F, quote=F, sep = "\t")
--- 6_Blastn_retrievingHumanlncRNA.R
+++ 6_Blastn_retrievingHumanlncRNA.R
@@ -2,7 +2,7 @@
 module load GNU/4.4.5 
 module load BEDTools/2.24.0  
 
-bedtools getfasta -s -fi equCab2.0_genome.fa -bed P.bed -name -fo P.fa
+bedtools getfasta -s -fi equCab2.0_genome.fa -bed P.bed -name -split -fo P.fa
 
 curl -O ftp://ftp.ensembl.org/pub/release-86/fasta/homo_sapiens/ncrna/Homo_sapiens.GRCh38.ncrna.fa.gz
 gunzip Homo_sapiens.GRCh38.ncrna.fa.gz
@@ -10,5 +10,5 @@ gunzip Homo_sapiens.GRCh38.ncrna.fa.gz
 module load GNU/4.8.3
 module load BLAST+/2.3.0
 
-makeblastdb -in Homo_sapiens.GRCh38.ncrna.fa -input_type fasta -dbtype nucl -parse_seqids -out hg38_ncrna_db
+makeblastdb -in Homo_sapiens.GRCh38.ncrna.fa -input_type fasta -dbtype nucl -out hg38_ncrna_db
 blastn -query P.fa  -db hg38_ncrna_db  -max_target_seqs 1 -outfmt "6 qseqid sseqid pident qcovs evalue" -evalue 1e-5 -num_threads 10 > lncRNA_hg_nt.outfmt6
