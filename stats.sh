ad1f1a803e27be82c4c8c693440c7b237127b715
drtamermansour <drtamermansour@gmail.com>
2016-12-08 00:41:54 -0500

    recode to use transcript name

--- stats.sh
+++ stats.sh
@@ -5,13 +5,13 @@ novel_I="inputs/novelAnn/sup/RNAseqSupTrans.merge.reduced.ORF_exons.candNovel.su
 novel_II="inputs/novelAnn/unsup.cons/RNAseqSupTrans.merge.reduced.ORF_exons.candNovel.unsup.cons"
 novel_III="inputs/novelAnn/unsup.uncons.ORF/RNAseqSupTrans.merge.reduced.ORF_exons.candNovel.unsup.uncons.ORF"
 intergenic="inputs/intergenic/unsup.ids"
-echo "group" "input_trans" "F1" "F2" "F3" > stats.txt
+echo "group" "input_trans" "F1" "F2" "F3" "F4"> stats.txt
 for gp in known_ncRNA novel_I novel_II novel_III intergenic;do 
  echo "$gp" "$(tail -n+2 ${!gp} | wc -l)" "$(cat F1_$gp | wc -l)" "$(cat F2_$gp | wc -l)" "$(cat ${gp}_P.bed | wc -l)" "$(cat F4_$gp | wc -l)";
 done >> stats.txt
 
 
-echo "group" "input_trans" "af_F1" "af_F2" "af_F3" > stats2.txt
+echo "group" "input_trans" "af_F1" "af_F2" "af_F3" "af_F4"> stats2.txt
 for gp in known_ncRNA novel_I novel_II novel_III intergenic;do 
  echo "$gp" "$(tail -n+2 ${!gp} | wc -l)" "no_file" "$(cat ${gp}_f2.bed | wc -l)" "$(cat ${gp}_f3.bed | wc -l)" "$(cat ${gp}_final.bed | wc -l)";
 done >> stats2.txt
